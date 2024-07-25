a9bdcb32d44d8796b5b34c0d904846440b81f069
datpham0412 <tiendat041202@gmail.com>
2024-07-25 10:19:24 +1000

    Promise: Create a promise

---
+++ Promise/createAPromise.js
@@ -0,0 +1,16 @@
+// Create a promise using the 'Promise' constructor, which takes a function (executor) with two arguments:
+// resolve and reject. The executor function is called immediately when the promise is created.
+
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const success = true; // Simulate success or failure
+        if (success) {
+            resolve('Promise was successful!');
+        }else{
+            reject('Promise was not successful!');
+        }
+    }, 1000);
+});
+
+console.log(promise);
