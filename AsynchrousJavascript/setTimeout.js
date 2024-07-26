e0a1a0f8808eb77c636e7c03abea011e8525f119
datpham0412 <tiendat041202@gmail.com>
2024-07-26 15:49:07 +1000

    Asynchronous: setTimeout

---
+++ AsynchrousJavascript/setTimeout.js
@@ -0,0 +1,9 @@
+// setTimeout is used to execute a function once after a specified delay
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Executed after 2 seconds");
+}, 2000);
+
+console.log("End");
